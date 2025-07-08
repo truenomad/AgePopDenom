787c42552c58b2f54b384eb6b177d20af32e88f4
Mohamed Yusuf <mohamedayusuf87@gmail.com>
2025-07-08 11:04:30 +0300

    Remove automap from your DESCRIPTION

--- R/setup.R
+++ R/setup.R
@@ -38,7 +38,7 @@ install_suggested_packages <- function(libname = NULL, pkgname = NULL) {
     "haven", "here", "matrixStats", "rstudioapi", "geodata",
     "pbmcapply", "remotes", "future", "future.apply",
     "testthat", "rdhs", "openxlsx2", "purrr", "rlang", "pak",
-    "sp", "automap", "knitr", "rmarkdown", "mockery"
+    "sp", "knitr", "rmarkdown", "mockery"
   )
 
   missing_pkgs <- suggested_pkgs[!sapply(
